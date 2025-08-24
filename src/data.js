0b3d7c767abc5d08daa5be8fb932fb01f9c2f1fe
Aman-Bam <mhub7058@gmail.com>
2025-08-25 01:31:31 +0530

    done

---
+++ src/data.js
@@ -0,0 +1,163 @@
+export const movies = [
+  {
+    "id": 533535,
+    "title": "Deadpool & Wolverine",
+    "release_date": "2024-07-24",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/yDHYTfa3R0jFvba16jBB1ef8otIt.jpg",
+    "poster_path": "https://upload.wikimedia.org/wikipedia/en/4/4c/Deadpool_%26_Wolverine_poster.jpg",
+    "category": "Action",
+  },
+  {
+    "id": 718821,
+    "title": "Twisters",
+    "release_date": "2024-07-10",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/5B6DzAVoKxLHyjXS8qNKSBE9Y.jpg",
+    "poster_path": "https://upload.wikimedia.org/wikipedia/en/2/24/Twisters_Official_US_Theatrical_Poster.jpg",
+    "category": "Thriller",
+  },
+  {
+    "id": 1022789,
+    "title": "Inside Out 2",
+    "release_date": "2024-06-11",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/example1.jpg",
+    "poster_path": "https://upload.wikimedia.org/wikipedia/en/f/f7/Inside_Out_2_poster.jpg",
+    "category": "Animation"
+  },
+  {
+    "id": 934526,
+    "title": "Gladiator II",
+    "release_date": "2024-11-22",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/example3.jpg",
+    "poster_path": "https://upload.wikimedia.org/wikipedia/en/0/04/Gladiator_II_%282024%29_poster.jpg",
+    "category": "Drama"
+  },
+  {
+    "id": 876543,
+    "title": "Avatar: Fire and Ash",
+    "release_date": "2025-12-19",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/example5.jpg",
+    "poster_path": "https://upload.wikimedia.org/wikipedia/en/4/43/Avatar_Fire_and_Ash_first_poster.jpeg",
+    "category": "Sci-Fi"
+  },
+  {
+    "id": 564738,
+    "title": "Mission Impossible: Reckoning Part Two",
+    "release_date": "2025-06-27",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/example7.jpg",
+    "poster_path": "https://upload.wikimedia.org/wikipedia/en/e/ed/Mission-_Impossible_%E2%80%93_Dead_Reckoning_Part_One_poster.jpg",
+    "category": "Action"
+  },
+  {
+    "id": 112233,
+    "title": "Joker: Folie à Deux",
+    "release_date": "2024-10-04",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/example9.jpg",
+    "poster_path": "https://upload.wikimedia.org/wikipedia/en/e/e8/Joker_-_Folie_%C3%A0_Deux_poster.jpg",
+    "category": "Horror"
+  },
+  {
+    "id": 223344,
+    "title": "Kung Fu Panda 4",
+    "release_date": "2024-03-08",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/example11.jpg",
+    "poster_path": "https://upload.wikimedia.org/wikipedia/en/7/7f/Kung_Fu_Panda_4_poster.jpg",
+    "category": "Animation"
+  },
+  {
+    "id": 334455,
+    "title": "The Marvels",
+    "release_date": "2023-11-10",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/example13.jpg",
+    "poster_path": "https://upload.wikimedia.org/wikipedia/en/7/7a/The_Marvels_poster.jpg",
+    "category": "Action"
+  },
+  {
+    "id": 445566,
+    "title": "Venom: The Last Dance",
+    "release_date": "2024-11-08",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/example15.jpg",
+    "poster_path": "https://upload.wikimedia.org/wikipedia/en/a/a3/Venom_The_Last_Dance_Poster.jpg",
+    "category": "Action"
+  },
+  {
+    "id": 556677,
+    "title": "Thor: The Dark World",
+    "release_date": "2013-10-08",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/example17.jpg",
+    "poster_path": "https://upload.wikimedia.org/wikipedia/en/7/7f/Thor_The_Dark_World_poster.jpg",
+    "category": "Action"
+  },
+  {
+    "id": 667788,
+    "title": "Captain America: The First Avenger",
+    "release_date": "2011-07-29",
+    "backdrop_path": "https:/image.tmdb.org/t/p/original/example19.jpg",
+    "poster_path": "https://resizing.flixster.com/ljLrpTy3USzTTB6Q21Fztrpz3Uc=/fit-in/180x240/v2/https://resizing.flixster.com/DPxL1sG2snpcMbqGJc67YGTrjlM=/ems.cHJkLWVtcy1hc3NldHMvbW92aWVzL2JmZTdkNDA5LTA5NzktNGE3OC04ODE2LTJlOTBkYTA3MDY2NS5qcGc=",
+    "category": "Action"
+  },
+  {
+    "id": 778899,
+    "title": "The Lion King Prequel",
+    "release_date": "2024-07-05",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/example21.jpg",
+    "poster_path": "https://upload.wikimedia.org/wikipedia/en/0/0b/Mufasa_The_Lion_King_Movie_2024.jpeg",
+    "category": "Drama"
+  },
+  {
+    "id": 889900,
+    "title": "Super Mario Bros. 2",
+    "release_date": "2026-04-03",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/example23.jpg",
+    "poster_path": "https://upload.wikimedia.org/wikipedia/en/7/78/Super_Mario_Bros._2_box_art.png",
+    "category": "Animation"
+  },
+  {
+    "id": 990011,
+    "title": "Avengers: Secret Wars",
+    "release_date": "2027-05-07",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/example25.jpg",
+    "poster_path": "https://upload.wikimedia.org/wikipedia/en/0/08/Secretwars1.png",
+    "category": "Animation"
+  },
+  {
+    "id": 101112,
+    "title": "Shrek 5",
+    "release_date": "2026-09-25",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/example27.jpg",
+    "poster_path": "https://upload.wikimedia.org/wikipedia/en/b/be/Shrek_5_teaser_poster_2024.jpg",
+    "category": "Animation"
+  },
+  {
+    "id": 121314,
+    "title": "The Batman Part II",
+    "release_date": "2025-10-03",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/example29.jpg",
+    "poster_path": "https://upload.wikimedia.org/wikipedia/en/f/ff/The_Batman_%28film%29_poster.jpg",
+    "category": "Action"
+  },
+  {
+    "id": 131415,
+    "title": "Spider-Man: Beyond the Spider-Verse",
+    "release_date": "2025-03-28",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/example31.jpg",
+    "poster_path": "https://upload.wikimedia.org/wikipedia/en/thumb/a/a0/Spider-Man_Beyond_the_Spider-Verse_logo.jpg/500px-Spider-Man_Beyond_the_Spider-Verse_logo.jpg",
+    "category": "Animation"
+  },
+  {
+    "id": 141516,
+    "title": "Toy Story 5",
+    "release_date": "2026-06-19",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/example33.jpg",
+    "poster_path": "https://upload.wikimedia.org/wikipedia/en/thumb/1/14/Toy_Story_5_logo.jpg/500px-Toy_Story_5_logo.jpg",
+    "category": "Drama"
+  },
+  {
+    "id": 151617,
+    "title": "Frozen 4",
+    "release_date": "2029-11-22",
+    "backdrop_path": "https://image.tmdb.org/t/p/original/example35.jpg",
+    "poster_path": "https://upload.wikimedia.org/wikipedia/en/thumb/7/70/Frozen_logo.svg/420px-Frozen_logo.svg.png",
+    "category": "Animation"
+  },
+  
+]
